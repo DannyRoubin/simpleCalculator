e2f796869675e39b8aadfcec1572df0a867be66b
Danny Roubin <dannyroubin1@gmail.com>
2022-06-29 11:46:53 -0700

    calculator works but I want to change it so that the user is able to make new calculations without having to restart each time

--- Calculator.cpp
+++ Calculator.cpp
@@ -4,6 +4,10 @@
 
 using namespace std;
 
+void math (float num1, float num2) {
+
+}
+
 int main() {
 
 
@@ -12,11 +16,30 @@ int main() {
 
 
 
-    cout << "enter an operator3: +, -, *,/ ";
+    cout << "enter an operator: +, -, *,/ ";
     cin >> op;
 
     cout << "Enter two numbers: ";
     cin >> number1 >> number2;
+
+    switch(op) {
+
+        case '+': 
+        cout << number1 << "+" << number2 << "=" << number1 + number2; 
+        break;
+
+        case '-': 
+        cout << number1 << "-" << number2 << "=" << number1 - number2; 
+        break;
+
+        case '*': 
+        cout << number1 << "*" << number2 << "=" << number1 * number2; 
+        break;
+
+        case '/': 
+        cout << number1 << "/" << number2 << "=" << number1 / number2; 
+        break;
+    }
 
 
 
