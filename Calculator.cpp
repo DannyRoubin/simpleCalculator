c09e310918c8dfa6e95369686651c4bf77ac6a4d
Danny Roubin <dannyroubin1@gmail.com>
2022-06-29 11:36:39 -0700

    added ability to add in an operator and two numbers

---
+++ Calculator.cpp
@@ -0,0 +1,24 @@
+//Calculator.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+    char op;
+    float number1, number2;
+
+
+
+    cout << "enter an operator3: +, -, *,/ ";
+    cin >> op;
+
+    cout << "Enter two numbers: ";
+    cin >> number1 >> number2;
+
+
+
+    
+}
